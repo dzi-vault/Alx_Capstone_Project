58b0189ae53bd9769160a9ec8b357750ea3a9bc3
Tina Tettey <adtettey01@gmail.com>
2026-02-20 21:21:48 +0000

    Add products app with models, serializer, views, and API endpoints

--- Lience/products/models.py
+++ Lience/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from manufacturers.models import ManufacturerProfile
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    manufacturer = models.ForeignKey(ManufacturerProfile, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
